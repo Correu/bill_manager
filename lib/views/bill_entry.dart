6725c61af5ca305b0067ef88e11fd98ec8b114d0
Tyler Correu <60193729+Correu@users.noreply.github.com>
2022-03-08 00:30:59 -0600

    basic layout changes

---
+++ lib/views/bill_entry.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BillEntry extends StatelessWidget {
+  const BillEntry({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Bill Entry Page');
+  }
+}
