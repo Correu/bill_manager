358b427a0d72511262a54e5749b857bef91d0371
Tyler Correu <60193729+Correu@users.noreply.github.com>
2022-03-20 12:57:08 -0500

    calendar addition, db connection

---
+++ lib/views/Models/Loans.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Loans {
+  Response? response;
+
+  Loans({this.response});
+
+  Loans.fromJson(Map<String, dynamic> json) {
+    response = json['response'] != null
+        ? Response.fromJson(json['response'])
+        : null;
+  }
+
+  static Future<Loans> fetchLoans() async {
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:8000/api/retrieveLoan'));
+
+    if (response.statusCode == 200) {
+      return Loans.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load loans');
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (response != null) {
+      data['response'] = response!.toJson();
+    }
+    return data;
+  }
+}
+
+class Response {
+  List<Results>? results;
+
+  Response({this.results});
+
+  Response.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Results {
+  int? id;
+  String? company;
+  int? amount;
+  String? rate;
+  int? timeFrame;
+  String? dueDay;
+
+  Results(
+      {this.id,
+      this.company,
+      this.amount,
+      this.rate,
+      this.timeFrame,
+      this.dueDay});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    company = json['company'];
+    amount = json['amount'];
+    rate = json['rate'];
+    timeFrame = json['time_frame'];
+    dueDay = json['due_day'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['company'] = company;
+    data['amount'] = amount;
+    data['rate'] = rate;
+    data['time_frame'] = timeFrame;
+    data['due_day'] = dueDay;
+    return data;
+  }
+}
--- lib/views/payment_list.dart
+++ lib/views/payment_list.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'Models/Loans.dart';
 
-class PaymentList extends StatelessWidget {
+class PaymentList extends StatefulWidget {
   const PaymentList({Key? key}) : super(key: key);
 
   @override
+  _PaymentListState createState() => _PaymentListState();
+}
+
+class _PaymentListState extends State<PaymentList> {
+  late Future<Loans> _loans;
+
+  @override
+  void initState() {
+    super.initState();
+    _loans = Loans.fetchLoans();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Payment List');
+    return FutureBuilder<Loans>(
+      future: _loans,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.response.results.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    '${snapshot.data.response.results[index].id}',
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 }
