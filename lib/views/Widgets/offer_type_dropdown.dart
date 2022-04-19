d7090cd6457a49dc1669531c705d420337c48598
Tyler Correu <60193729+Correu@users.noreply.github.com>
2022-04-18 22:11:20 -0500

    Update offer_type_dropdown.dart

--- lib/views/Widgets/offer_type_dropdown.dart
+++ lib/views/Widgets/offer_type_dropdown.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
-class OfferTypeDropdown extends StatelessWidget {
+class OfferTypeDropdown extends StatefulWidget {
   const OfferTypeDropdown({Key? key}) : super(key: key);
 
   @override
+  _OfferTypeDropdownState createState() => _OfferTypeDropdownState();
+}
+
+class _OfferTypeDropdownState extends State<OfferTypeDropdown> {
+  String dropdownValue = 'One';
+
+  @override
   Widget build(BuildContext context) {
-    return const Text(
-      'this is text',
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.blue),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
